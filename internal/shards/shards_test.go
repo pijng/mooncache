1f1ce73eaf69db76c9245ac8453ae7ce0b0ceee2
Mark <shalpack@gmail.com>
2023-02-23 18:03:02 +0300

    Tests for shards

--- internal/shards/shards_test.go
+++ internal/shards/shards_test.go
@@ -113,12 +113,44 @@ func TestDel(t *testing.T) {
 		args args
 		err  error
 	}{
-		{"item should not be present in cache after delete", args{key: "item1337", value: value{deep: wantValue}, cost: 0, ttl: math.MaxInt64}, lib.ValueNotPresent()},
+		{"item should not be present in cache after delete by string key", args{key: "item1337", value: value{deep: wantValue}, cost: 0, ttl: math.MaxInt64}, lib.ValueNotPresent()},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			_ = Set(tt.args.key, tt.args.value, tt.args.cost, tt.args.ttl)
 			Del(tt.args.key)
+			got, err := Get(tt.args.key)
+			assert.Equal(t, tt.err, err)
+			assert.Nil(t, got)
+		})
+	}
+}
+
+func TestDelByHash(t *testing.T) {
+	buildCache()
+
+	type value struct {
+		deep string
+	}
+	type args struct {
+		key   string
+		value interface{}
+		cost  int
+		ttl   int64
+	}
+	wantValue := "01001111 01101000 00111111 00100000 01011001 01101111 01110101 00100111 01110010 01100101 00100000 01000001 01110000 01110000 01110010 01101111 01100001 01100011 01101000 01101001 01101110 01100111 00100000 01001101 01100101 00111111"
+	tests := []struct {
+		name      string
+		args      args
+		hashedKey uint64
+		err       error
+	}{
+		{"item should not be present in cache after delete by hashed key", args{key: "Pelinal Whitestrake is a cyborg proofs", value: value{deep: wantValue}, cost: 0, ttl: math.MaxInt64}, 14388442895772204505, lib.ValueNotPresent()},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_ = Set(tt.args.key, tt.args.value, tt.args.cost, tt.args.ttl)
+			DelByHash(tt.hashedKey)
 			got, err := Get(tt.args.key)
 			assert.Equal(t, tt.err, err)
 			assert.Nil(t, got)
